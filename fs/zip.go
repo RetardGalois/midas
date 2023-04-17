e6bb58a1d4d54324d9e2df714c87bc7778be9bb7
RetardGalois <79499397+RetardGalois@users.noreply.github.com>
2023-04-17 20:58:03 -0300

    add support for password protected zip. using a hardcoded password list for this.

--- fs/zip.go
+++ fs/zip.go
@@ -2,11 +2,64 @@ package fs
 
 import (
 	"io/ioutil"
-	"archive/zip"
+	"github.com/yeka/zip"
 	"strings"
+	"errors"
 )
 
-func readZip(file *zip.File) (string, error) {
+var passwords = []string{
+	"Borwita",
+	"GoldenEagleCloud",
+	"Free",
+	"OTTOMAN",
+	"@Daisy_Cloud",
+	"ygygg777",
+	"OTTOMAN**1115",
+	"OTTOLIKE",
+	"@cheatbreakercloud",
+	"https://t.me/MariaLogs",
+	"@crypton_logs",
+	"@TOR_LOG",
+	"@RaccoonCloud",
+	"@FASTCLOUDD",
+	"@observercloud",
+	"@MikaLogsfree",
+	"https://t.me/REDLINEPREMIUM/",
+	"https://t.me/+WlFsI97nclo2ZjJl",
+	"@freshlogs1only",
+	"LOGS",
+	"PASS",
+	"redlinevip",
+	"@freshlogs1only",
+	"t.me/stake_logs",
+	"https://t.me/Zeuscloudfree",
+	"@NoxyCloud",
+	"@segacloud",
+	"@spiderLogs",
+	"https://t.me/EMClouds",
+	"https://t.me/PremCloud",
+	"https://t.me/prdscloud",
+	"https://t.me/magicianlogs",
+	"https://t.me/tor_log",
+	"https://t.me/crypton_logs",
+	"https://t.me/Wooden_Cloud",
+	"@tokyocloudd",
+	"https://t.me/Heavenlogscloud",
+	"@milkywaycloud",
+	"https://t.me/tor_log",
+	"t.me/xl0gs",
+	"@sigmcloud",
+	"@zet_cloud",
+	"https://t.me/klaus_cloud_public",
+	"https://t.me/RedlineClouds1",
+	"https://t.me/ObserverPrivate",
+}
+
+
+func readZip(file *zip.File, password string) (string, error) {
+	if password != "" {
+		file.SetPassword(password)
+	}
 	fc, err := file.Open()
 	if err != nil {
 		return "", err
@@ -19,6 +72,17 @@ func readZip(file *zip.File) (string, error) {
 	return string(content), nil
 }
 
+func getPass(file *zip.File) (string, error) {
+	for _, pass := range passwords {
+		file.SetPassword(pass)
+		if _, err := readZip(file, pass); err == nil {
+			return pass, nil
+		}
+	}
+	return "", errors.New("can't guess the password.")
+}
+
+
 func ListZip(zip_file string) ([]string, error) {
 	ret := []string{}
 	zf, err := zip.OpenReader(zip_file)
@@ -27,11 +91,20 @@ func ListZip(zip_file string) ([]string, error) {
 	}
 	defer zf.Close()
 
+	possible_pass := ""
 	for _, file := range zf.File {
 		splited_path := strings.Split(file.Name, "/")
 		file_name := strings.TrimSuffix(splited_path[len(splited_path)-1], "\n")
 		if file_regex.MatchString(file_name) {
-			content, err := readZip(file)
+			if (file.IsEncrypted() && possible_pass == "") {
+				guessed_pass, err := getPass(file)
+				if err == nil {
+					possible_pass = guessed_pass
+				} else {
+					return nil, err
+				}
+			}
+			content, err := readZip(file, possible_pass)
 			if err != nil {
 				//panic(fmt.Sprintf("Can't read file. ERROR: %s", err))
 				continue
--- go.mod
+++ go.mod
@@ -14,5 +14,7 @@ require (
 	github.com/pierrec/lz4/v4 v4.1.14 // indirect
 	github.com/therootcompany/xz v1.0.1 // indirect
 	github.com/ulikunitz/xz v0.5.10 // indirect
-	golang.org/x/text v0.3.7 // indirect
+	github.com/yeka/zip v0.0.0-20180914125537-d046722c6feb // indirect
+	golang.org/x/crypto v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
 )
